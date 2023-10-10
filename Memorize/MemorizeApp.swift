443a47d8bf0c2ada029af3cbf9ebd0fac6317deb
theeramukt <chayanit9336@gmail.com>
2023-10-10 15:11:39 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by ชญานิษฐ์ โลหะกิจจา on 10/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
